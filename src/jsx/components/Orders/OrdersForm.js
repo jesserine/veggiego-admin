825af73751f38c0b8a2645337809bd88cdc55708
Jesserine Lopez <dev.jesserinelopez@gmail.com>
2021-08-14 02:15:47 +0800

    Adds autocomplete search on products.

--- src/jsx/components/Orders/OrdersForm.js
+++ src/jsx/components/Orders/OrdersForm.js
@@ -7,133 +7,188 @@ import { v4 as uuid } from 'uuid'
 import PageTitle from "../../layouts/PageTitle";
 import { SplitButton, ButtonGroup, Dropdown } from "react-bootstrap";
 import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
+import { set } from "date-fns";
 
 const OrdersForm = (props) => {
-   const initialFieldValues = {
-      notes: '',
-      dateAdded: new Date().toLocaleString(),
-   }
-
-   var [values, setValues] = useState(initialFieldValues)
-   var [unitObjects, setUnitObjects] = useState({})
-   var [product, setProduct] = useState('');
-
-   useEffect(() => {
-    if(product.length > 0)
-    {
-        
+    const initialFieldValues = {
+        notes: '',
+        dateAdded: new Date().toLocaleString(),
     }
-   }, [product])
 
-   useEffect(() => {
-      firebaseDb.ref('orders/').on('value', (snapshot) => {
-        if (snapshot.val() != null)
-        setUnitObjects({
-            ...snapshot.val(),
-          })
-        else setUnitObjects({})
-      })
+    var [values, setValues] = useState(initialFieldValues)
+    var [unitObjects, setUnitObjects] = useState({})
+    var [product, setProduct] = useState('');
+    var [result, setResult] = useState([]);
+    var [value, setValue] = useState('');
+
+
+    useEffect(() => {
+        if (value.length > 0) {
+            firebaseDb.ref('products/').on('value', (snapshot) => {
+                if (snapshot.val() != null) {
+                    const productsDb = snapshot.val()
+                    setResult([])
+                    let searchQuery = value.toLowerCase();
+                    for (let id in productsDb) {
+                        let fruit = productsDb[id].productName.toLowerCase();
+                        if (fruit.slice(0, searchQuery.length).indexOf(searchQuery) !== -1) {
+                            setResult(prevResult => {
+                                return [...prevResult, productsDb[id].productName]
+                            });
+                        }
+                    }
+                } else {
+                    setResult([])
+                }
+            })
+        } else {
+            setResult([])
+        }
+    }, [value])
+
+
+    //    useEffect(() => {
+    //     if(product.length > 0)
+    //     {
+    //         fetch('products/').then(
+    //             response => response.json()
+    //         ).then( responseData => {
+    //             setResult([]);
+    //             let searchQuery = product.toLowerCase();
+    //             for(const key in responseData){
+    //                 let fruit = responseData[key].productName.toLowerCase();
+    //                 if(fruit.slice(0, searchQuery.length).indexOf(searchQuery) !== -1){
+    //                     setResult( prevResult => {
+    //                         return[...prevResult, responseData[key].productName]
+    //                     });
+    //                 }
+    //             }
+    //         }).catch( error =>{
+    //             console.log(error);
+    //         })
+    //     } else {
+    //         setResult([]);
+    //     }
+    //    }, [product])
+
+    useEffect(() => {
+        firebaseDb.ref('orders/').on('value', (snapshot) => {
+            if (snapshot.val() != null)
+                setUnitObjects({
+                    ...snapshot.val(),
+                })
+            else setUnitObjects({})
+        })
     }, [])
 
 
-  useEffect(() => {
-   if (props.currentId == '')
-     setValues({
-       ...initialFieldValues,
-     })
-   else
-     setValues({
-       ...props.unitObjects[props.currentId],
-     })
- }, [props.currentId, props.unitObjects])
+    useEffect(() => {
+        if (props.currentId == '')
+            setValues({
+                ...initialFieldValues,
+            })
+        else
+            setValues({
+                ...props.unitObjects[props.currentId],
+            })
+    }, [props.currentId, props.unitObjects])
 
- const handleInputChange = (e) => {
-   var { name, value } = e.target
-   setValues({
-     ...values,
-     [name]: value,
-   })
- }
+    const handleInputChange = (e) => {
+        var { name, value } = e.target
+        setValues({
+            ...values,
+            [name]: value,
+        })
+    }
 
- const [imageUrl, setImageUrl] = useState()
- const readImages = async (e) => {
-   const file = e.target.files[0]
-   const id = uuid()
-   const imagesRef = storage.ref('images').child(id)
+    const [imageUrl, setImageUrl] = useState()
+    const readImages = async (e) => {
+        const file = e.target.files[0]
+        const id = uuid()
+        const imagesRef = storage.ref('images').child(id)
 
-   await imagesRef.put(file)
-   imagesRef.getDownloadURL().then((url) => {
-     setImageUrl(url)
-   })
- }
+        await imagesRef.put(file)
+        imagesRef.getDownloadURL().then((url) => {
+            setImageUrl(url)
+        })
+    }
 
- if (typeof imageUrl !== 'undefined' && imageUrl != null) {
-   values.productImage = imageUrl
- } 
+    if (typeof imageUrl !== 'undefined' && imageUrl != null) {
+        values.productImage = imageUrl
+    }
 
- const handleFormSubmit = (e) => {
-    console.log("inside handleFormSubmit")
-   e.preventDefault()
-   props.addOrEdit(values)
-   window.location.reload(false)
- }
+    const handleFormSubmit = (e) => {
+        console.log("inside handleFormSubmit")
+        e.preventDefault()
+        props.addOrEdit(values)
+        window.location.reload(false)
+    }
 
- const enabled = values.notes!=null
-   return (
-      <Fragment>
-         <div className="row">
-            <div className="col-xl-12 col-lg-12">
-               <div className="card">
-                  <div className="card-header">
-                     <h4 className="card-title">{props.currentId === '' ? 'Add' : 'Update'} Unit</h4>
-                  </div>
-                  <div className="card-body">
-                     <div className="basic-form">
-                        <form onSubmit={handleFormSubmit}>
-                           <div className="form-row">
-                              <div className="form-group col-md-12">
-                                 <label>Notes</label>
-                                 <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Notes"
-                                    name='notes'
-                                    value={values.notes}
-                                    onChange={handleInputChange}
-                                    required
-                                 />
-                              </div>
+    const enabled = values.notes != null
+    return (
+        <Fragment>
+            <div className="row">
+                <div className="col-xl-12 col-lg-12">
+                    <div className="card">
+                        <div className="card-header">
+                            <h4 className="card-title">{props.currentId === '' ? 'Add' : 'Update'} Unit</h4>
+                        </div>
+                        <div className="card-body">
+                            <div className="basic-form">
+                                <form onSubmit={handleFormSubmit}>
+                                    <div className="form-row">
+                                        <div className="form-group col-md-12">
+                                            <label>Notes</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                placeholder="Notes"
+                                                name='notes'
+                                                value={values.notes}
+                                                onChange={handleInputChange}
+                                                required
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="form-group col-md-12">
+                                            <label>Product</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                placeholder="Product"
+                                                name='product'
+                                                value={value}
+                                                onChange={(event) => setValue(event.target.value)}
+                                                required
+                                            />
+                                            <div className="searchBack">
+                                                {result.map((result, index) => (
+                                                    <a href="orders" id={index}>
+                                                    <div className="searchEntry">   
+                                                        {result}
+                                                    </div>
+                                                    </a>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="form-group col-md-12">
+                                            <input type="submit"
+                                                value={props.currentId == '' ? 'Save' : 'Update'}
+                                                className="btn btn-primary btn-block"
+                                                disabled={!enabled} />
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
-                            <div className="form-row">
-                              <div className="form-group col-md-12">
-                                 <label>Product</label>
-                                 <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Notes"
-                                    name='notes'
-                                    value={product}
-                                    onChange={(event) => setProduct(event.target.value)}
-                                    required
-                                 />
-                              </div>
-                            </div>
-                           <div className="form-row">
-                              <div className="form-group col-md-12">
-                                 <input type="submit" 
-                                 value = {props.currentId == '' ? 'Save' : 'Update'}
-                                 className="btn btn-primary btn-block"
-                                 disabled={!enabled} />
-                              </div>
-                           </div>
-                        </form>
-                     </div>
-                  </div>
-               </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </Fragment>
-   );
+        </Fragment>
+    );
 };
 
 export default OrdersForm;
