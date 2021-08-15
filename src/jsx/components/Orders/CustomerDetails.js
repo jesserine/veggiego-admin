3f697555e32b19db2d057edc965452d8b5560e92
Jesserine Lopez <dev.jesserinelopez@gmail.com>
2021-08-16 02:06:29 +0800

    Updates customer details page.

--- src/jsx/components/Orders/CustomerDetails.js
+++ src/jsx/components/Orders/CustomerDetails.js
@@ -18,16 +18,36 @@ const CustomerDetails = () => {
                   />
                </div>
             </div>
+         </div>
+         <div className="row">
             <div className="col-lg-12 mb-2">
-               <div className="form-group">
-                  <label className="text-label">Date of Delivery* </label>
-                  <span>
-                  <DatePicker required /></span>
+               <div className="form-group" >
+                  <label className="text-label">Address Details*</label>
+                  <select
+                     defaultValue='Select Address'
+                     id="inputState"
+                     className="form-control"
+                     name='unit'
+                     required
+                  >
+                     <option value='Unit'>Choose Unit..</option>
+                     <option value='Address 1'>Address 1</option>
+                     <option value='Address 2'>Address 2</option>
+                     <option value='Address 3'>Address 3</option>
+                  </select>
                </div>
             </div>
-            <div className="col-lg-12 mb-2">
+         </div>
+         <div className="row">
+            <div className="col-lg-2 mb-2" align="left">
+               <div className="form-group" >
+                  <label className="text-label">Date of Delivery* </label>
+                  <div><DatePicker required align="left"/></div>
+               </div>
+            </div>
+            <div className="col-lg-6 mb-2">
                <div className="form-group">
-                  <label className="text-label">Address Details*</label>
+                  <label className="text-label">Delivery Fee*</label>
                   <select
                      defaultValue='Select Address'
                      id="inputState"
