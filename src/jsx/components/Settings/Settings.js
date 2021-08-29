dd874b163d1b82e0e7ebac954e3e0a56c995eea1
Jesserine Lopez <dev.jesserinelopez@gmail.com>
2021-08-29 22:32:35 +0800

    Updated settings page for the unit, category, and delivery forms.

--- src/jsx/components/Settings/Settings.js
+++ src/jsx/components/Settings/Settings.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import firebaseDb from "../../../firebase";
 import PageTitle from "../../layouts/PageTitle";
 import swal from "sweetalert";
@@ -16,208 +17,43 @@ import {
   ProgressBar,
 } from "react-bootstrap";
 
-/// imge
-import avatar1 from "../../../images/avatar/1.jpg";
-import avatar2 from "../../../images/avatar/2.jpg";
-import avatar3 from "../../../images/avatar/3.jpg";
-import { Link } from "react-router-dom";
-import RidersForm from "../Riders/RidersForm";
+
 
 const Settings = () => {
-  var [riderObjects, setRiderObjects] = useState({});
-  var [currentId, setCurrentId] = useState("");
-  var [searchTerm, setSearchTerm] = useState("");
-
-  const svg1 = (
-    <svg width="20px" height="20px" viewBox="0 0 24 24" version="1.1">
-      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <rect x="0" y="0" width="24" height="24"></rect>
-        <circle fill="#000000" cx="5" cy="12" r="2"></circle>
-        <circle fill="#000000" cx="12" cy="12" r="2"></circle>
-        <circle fill="#000000" cx="19" cy="12" r="2"></circle>
-      </g>
-    </svg>
-  );
-
-  useEffect(() => {
-    if (searchTerm.length > 0) {
-      firebaseDb.ref("riders/").on("value", (snapshot) => {
-        if (snapshot.val() != null) {
-          const ridersDb = snapshot.val();
-          setRiderObjects([]);
-          let searchQuery = searchTerm.toLowerCase();
-          for (let id in ridersDb) {
-            let rider = ridersDb[id].riderName.toLowerCase();
-            if (
-              rider.slice(0, searchQuery.length).indexOf(searchQuery) !== -1
-            ) {
-              setRiderObjects((prevResult) => {
-                return [...prevResult, ridersDb[id]];
-              });
-            }
-          }
-        } else {
-          setRiderObjects([]);
-        }
-      });
-    } else {
-      firebaseDb.ref("riders/").on("value", (snapshot) => {
-        if (snapshot.val() != null)
-          setRiderObjects({
-            ...snapshot.val(),
-          });
-        else setRiderObjects({});
-      });
-    }
-  }, [searchTerm]);
-
-  useEffect(() => {
-    firebaseDb.ref("riders/").on("value", (snapshot) => {
-      if (snapshot.val() != null)
-        setRiderObjects({
-          ...snapshot.val(),
-        });
-      else setRiderObjects({});
-    });
-  }, []);
-
-  const addOrEdit = (obj) => {
-    if (currentId === "") {
-      swal("Nice!", "A new rider profile is added!", "success");
-      firebaseDb.ref("riders/").push(obj, (err) => {
-        if (err) console.log(err);
-        else setCurrentId("");
-      });
-    } else {
-      swal("Nice!", "This rider profile is updated!", "success");
-      firebaseDb.ref(`riders/${currentId}`).set(obj, (err) => {
-        if (err) console.log(err);
-        else setCurrentId("");
-      });
-    }
-  };
-
-  const onDelete = (key) => {
-    swal({
-      title: "Are you sure?",
-      text: "Once deleted, you will not be able to recover this rider!",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    }).then((willDelete) => {
-      if (willDelete) {
-        firebaseDb.ref(`riders/${key}`).remove((err) => {
-          if (err) console.log(err);
-          else setCurrentId("");
-        });
-        swal("Poof! This rider profile has been deleted!", {
-          icon: "success",
-        });
-      } else {
-        swal("Your rider profile is safe!");
-      }
-    });
-  };
 
   return (
     <Fragment>
-      <div className="row">
-        <div className="col-xl-4 col-lg-4">
-          <PerfectScrollbar>
-            <RidersForm {...{ addOrEdit, currentId, riderObjects }} />
-            <RidersForm {...{ addOrEdit, currentId, riderObjects }} />
-          </PerfectScrollbar>
+      <div className="form-row col-md-12" >
+        <div className="form-group col-md-3 mt-4 mt-6">
+          <Link
+            to={{
+              pathname: "/products-category",
+            }}
+            className="btn btn-secondary btn-block"
+          >
+            Category
+          </Link>
         </div>
-        <div className="col-xl-8 col-lg-8">
-          <Row>
-            <Col lg={12}>
-              <Card>
-                <Card.Header>
-                  <Card.Title>My Riders</Card.Title>
-                  <Button
-                    variant="primary btn-rounded"
-                    onClick={() => {
-                      setCurrentId("");
-                    }}
-                  >
-                    <span className="btn-icon-left text-primary">
-                      <i className="fa fa-plus" />
-                    </span>
-                    Add
-                  </Button>
-                </Card.Header>
-                <Card.Body>
-                  <div className="search_bar dropdown show mb-3">
-                    <div className="dropdown-menushow">
-                      <form onSubmit={(e) => e.preventDefault()}>
-                        <input
-                          className="form-control"
-                          type="search"
-                          placeholder="Search Rider name"
-                          aria-label="Search"
-                          onChange={(event) =>
-                            setSearchTerm(event.target.value)
-                          }
-                        />
-                      </form>
-                    </div>
-                  </div>
-                  <Table responsive hover>
-                    <thead>
-                      <tr>
-                        <th>
-                          <strong>NAME</strong>
-                        </th>
-                        <th>
-                          <strong>CONTACT #</strong>
-                        </th>
-                        <th>
-                          <strong>ADDRESS</strong>
-                        </th>
-                        <th>
-                          <strong>VEHICLE TYPE</strong>
-                        </th>
-                        <th>
-                          <strong>VEHICLE PLATE #</strong>
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {Object.keys(riderObjects).map((id) => {
-                        return (
-                          <tr
-                            key={id}
-                            onClick={() => {
-                              setCurrentId(id);
-                            }}
-                          >
-                            <td>
-                              <img
-                                src={riderObjects[id].riderImage}
-                                className="rounded-lg mr-2"
-                                width="24"
-                                alt=""
-                              />
-                              <span>{riderObjects[id].riderName}</span>
-                            </td>
-                            <td>{riderObjects[id].riderContactNum}</td>
-                            <td>{riderObjects[id].riderAddress}</td>
-                            <td>{riderObjects[id].vehicleType}</td>
-                            <td>{riderObjects[id].vehiclePlateNum}</td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </Table>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+        <div className="form-group col-md-3 mt-4 mt-6">
+          <Link
+            to={{
+              pathname: "/products-unit",
+            }}
+            className="btn btn-info btn-block"
+          >
+            Unit
+          </Link>
         </div>
-      </div>
-
-      <div className="row">
-        <div className="col-xl-4 col-lg-4"></div>
+        <div className="form-group col-md-3 mt-4 mt-6">
+          <Link
+            to={{
+              pathname: "/orders-deliveryfee",
+            }}
+            className="btn btn-warning btn-block"
+          >
+            Delivery Fee
+          </Link>
+        </div>
       </div>
     </Fragment>
   );
