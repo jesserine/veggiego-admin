f8aad4bb8f4cafe51c67d0eda5b147d0d918e86a
Jesserine Lopez <dev.jesserinelopez@gmail.com>
2021-09-05 04:29:17 +0800

    Added save and update buttons.

--- src/jsx/components/Orders/AddRiderToOrderForm.js
+++ src/jsx/components/Orders/AddRiderToOrderForm.js
@@ -28,6 +28,7 @@ const AddRiderToOrderForm = (props) => {
   var [currentId, setCurrentId] = useState("");
   var [riderValues, setRiderValues] = useState({});
 
+  const [showUpdateButton, setShowUpdateButton] = useState(false);
   const [viewMode, setViewMode] = useState(false);
   const [imageUrl, setImageUrl] = useState();
 
@@ -63,7 +64,7 @@ const AddRiderToOrderForm = (props) => {
         rider: selectedRiderOption.rider,
       }));
     }
-  });
+  }, [selectedRiderOption]);
 
   // styles for combobox
   const customStyles = {
@@ -86,11 +87,13 @@ const AddRiderToOrderForm = (props) => {
   useEffect(() => {
     if (props.currentId === "") {
       setViewMode(false);
+      setShowUpdateButton(false);
       setValues({
         ...initialOrderFieldValues,
       });
     } else {
       setViewMode(true);
+      setShowUpdateButton(true);
       setValues({
         ...props.orderValues[props.currentId],
       });
@@ -136,117 +139,140 @@ const AddRiderToOrderForm = (props) => {
               <h4>
                 <strong>Invoice</strong> {props.currentId}{" "}
               </h4>
+              <span className="float-right">
+                {showUpdateButton && (
+                  <Link
+                    to={{
+                      pathname: "/customer-order",
+                      state: {
+                        order: values,
+                      },
+                    }}
+                    className="btn-sm btn-warning btn-block"
+                  >
+                    Update Order
+                  </Link>
+                )}
+              </span>
             </div>
             <div className="card-body">
-              <div className="row">
-                <div className="col-xl-6 col-sm-6 mb-4">
-                  <h5>Customer:</h5>
-                  <div>
-                    {" "}
-                    <strong>{values.customer.name}</strong>{" "}
+              <form onSubmit={handleFormSubmit}>
+                <div className="row">
+                  <div className="col-xl-6 col-sm-6 mb-4">
+                    <h5>Customer:</h5>
+                    <div>
+                      {" "}
+                      <strong>{values.customer.name}</strong>{" "}
+                    </div>
+                    <div>Phone: {values.customer.contactNumber}</div>
+                    <div>Address: {values.customer.address}</div>
+                    <div>Landmark: {values.customer.landmark}</div>
                   </div>
-                  <div>Phone: {values.customer.contactNumber}</div>
-                  <div>Address: {values.customer.address}</div>
-                  <div>Landmark: {values.customer.landmark}</div>
+                  <div className="col-xl-6 col-sm-6 mb-4">
+                    <h5>Date of Delivery:</h5>
+                    <div>
+                      {" "}
+                      <strong>{values.dateOfDelivery}</strong>{" "}
+                    </div>
+                    <br />
+                    <h5>Status:</h5>
+                    <div>
+                      {" "}
+                      <strong>{values.status}</strong>{" "}
+                    </div>
+                  </div>
                 </div>
-                <div className="col-xl-6 col-sm-6 mb-4">
-                  <h5>Date of Delivery:</h5>
-                  <div>
-                    {" "}
-                    <strong>{values.dateOfDelivery}</strong>{" "}
-                  </div>
-                  <br />
-                  <h5>Status:</h5>
-                  <div>
-                    {" "}
-                    <strong>{values.status}</strong>{" "}
-                  </div>
-                </div>
-              </div>
-              <br />
-              <div className="table-responsive">
-                <table className="table table-striped">
-                  <thead>
-                    <tr>
-                      <th className="center">#</th>
-                      <th>Product</th>
-                      <th>Quantity</th>
-                      <th>Unit</th>
-                      <th className="right">Price</th>
-                      <th className="center">Discount</th>
-                      <th className="right">SubTotal</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {values.products &&
-                      values.products.map((product, index) => (
-                        <tr>
-                          <td className="center">
-                            {Number(index) + Number(1)}
-                          </td>
-                          <td className="left strong">{product.productName}</td>
-                          <td className="left">{product.productQty}</td>
-                          <td className="left">{product.productUnit}</td>
-                          <td className="right">P {product.productPrice}</td>
-                          <td className="center">{product.discount} %</td>
-                          <td className="right">
-                            P {product.subtotal && product.subtotal}
-                          </td>
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>
-              </div>
-              <div className="row">
-                <div className="col-lg-4 col-sm-5"> </div>
-                <div className="col-lg-4 col-sm-5 ml-auto">
-                  <table className="table table-clear">
+                <br />
+                <div className="table-responsive">
+                  <table className="table table-striped">
+                    <thead>
+                      <tr>
+                        <th className="center">#</th>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th>Unit</th>
+                        <th className="right">Price</th>
+                        <th className="center">Discount</th>
+                        <th className="right">SubTotal</th>
+                      </tr>
+                    </thead>
                     <tbody>
-                      <tr>
-                        <td className="left">
-                          <strong>Total</strong>
-                        </td>
-                        <td className="right">P{values.total}</td>
-                      </tr>
-                      <tr>
-                        <td className="left">
-                          <strong>Delivery Fee</strong>
-                        </td>
-                        <td className="right">P{values.deliveryFee}</td>
-                      </tr>
-                      <tr>
-                        <td className="left">
-                          <strong>Grand Total</strong>
-                        </td>
-                        <td className="right">
-                          <strong>P{values.grandTotal}</strong>
-                        </td>
-                      </tr>
+                      {values.products &&
+                        values.products.map((product, index) => (
+                          <tr>
+                            <td className="center">
+                              {Number(index) + Number(1)}
+                            </td>
+                            <td className="left strong">
+                              {product.productName}
+                            </td>
+                            <td className="left">{product.productQty}</td>
+                            <td className="left">{product.productUnit}</td>
+                            <td className="right">P {product.productPrice}</td>
+                            <td className="center">{product.discount} %</td>
+                            <td className="right">
+                              P {product.subtotal && product.subtotal}
+                            </td>
+                          </tr>
+                        ))}
                     </tbody>
                   </table>
                 </div>
-              </div>
-              <br />
-              <div className="col-xl-12 col-sm-6 mb-4">
-                <h5>Notes:</h5>
-                <div>{values.notes}</div>
-              </div>
-              <br />
-              <div className="col-xl-12 col-sm-6 mb-4">
-                <h5>Rider:</h5>
-                <Select
-                  className={"form-control"}
-                  defaultValue={selectedRiderOption}
-                  onChange={setSelectedRiderOption}
-                  options={riderOptions}
-                  styles={customStyles}
-                  components={{
-                    DropdownIndicator: () => null,
-                    IndicatorSeparator: () => null,
-                  }}
-                />
-              </div>
-              <div className="col-xl-12 col-sm-6 mb-4"></div>
+                <div className="row">
+                  <div className="col-lg-4 col-sm-5"> </div>
+                  <div className="col-lg-4 col-sm-5 ml-auto">
+                    <table className="table table-clear">
+                      <tbody>
+                        <tr>
+                          <td className="left">
+                            <strong>Total</strong>
+                          </td>
+                          <td className="right">P{values.total}</td>
+                        </tr>
+                        <tr>
+                          <td className="left">
+                            <strong>Delivery Fee</strong>
+                          </td>
+                          <td className="right">P{values.deliveryFee}</td>
+                        </tr>
+                        <tr>
+                          <td className="left">
+                            <strong>Grand Total</strong>
+                          </td>
+                          <td className="right">
+                            <strong>P{values.grandTotal}</strong>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <br />
+                <div className="col-xl-12 col-sm-6 mb-4">
+                  <h5>Notes:</h5>
+                  <div>{values.notes}</div>
+                </div>
+                <br />{" "}
+                <div className="col-xl-12 col-sm-6 mb-4">
+                  <h5>Rider:</h5>
+                  <Select
+                    className={"form-control"}
+                    defaultValue={selectedRiderOption}
+                    onChange={setSelectedRiderOption}
+                    options={riderOptions}
+                    styles={customStyles}
+                    components={{
+                      DropdownIndicator: () => null,
+                      IndicatorSeparator: () => null,
+                    }}
+                  />
+                </div>
+                <div className="col-xl-12 col-sm-6 mb-4">
+                  <Button className="mt-4" variant="primary" type="submit">
+                    Save Rider Change
+                  </Button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
