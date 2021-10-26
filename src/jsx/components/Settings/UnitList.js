769567a682aec390f184ff37dcb23e13e0844870
Jesserine Lopez <dev.jesserinelopez@gmail.com>
2021-10-27 03:13:21 +0800

    Add search product unit.

--- src/jsx/components/Settings/UnitList.js
+++ src/jsx/components/Settings/UnitList.js
@@ -5,20 +5,23 @@ import { Row, Col, Card, Table, Badge, Button } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
 import UnitForm from "./UnitForm";
+import { useDataContext } from "../../../contexts/DataContext";
 
 const UnitList = () => {
   var [unitObjects, setUnitObjects] = useState({});
   var [currentId, setCurrentId] = useState("");
+  var [searchTerm, setSearchTerm] = useState("");
+  const { unitList, setUnitList } = useDataContext();
 
-  useEffect(() => {
-    firebaseDb.ref("unit/").on("value", (snapshot) => {
-      if (snapshot.val() != null)
-        setUnitObjects({
-          ...snapshot.val(),
-        });
-      else setUnitObjects({});
-    });
-  }, []);
+  // useEffect(() => {
+  //   firebaseDb.ref("unit/").on("value", (snapshot) => {
+  //     if (snapshot.val() != null)
+  //       setUnitObjects({
+  //         ...snapshot.val(),
+  //       });
+  //     else setUnitObjects({});
+  //   });
+  // }, []);
 
   const addOrEdit = (obj) => {
     console.log("inside addOrEdit");
@@ -46,15 +49,24 @@ const UnitList = () => {
     }
   };
 
+  const filteredUnit = (unitList, searchTerm) => {
+    if (!searchTerm) {
+      return unitList;
+    }
+    return Object.keys(unitList)
+      .filter((id) =>
+        unitList[id].unitName.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((res, key) => ((res[key] = unitList[key]), res), {});
+  };
+
   return (
     <Fragment>
       <div className="row">
         <div className="col-xl-12 col-lg-12">
           <h2 className="text-black font-w600 mb-1">Product Unit Settings</h2>
           <div className="mt-4">
-            <UnitForm
-              {...{ addOrEdit, currentId, setCurrentId, unitObjects }}
-            />
+            <UnitForm {...{ addOrEdit, currentId, setCurrentId, unitList }} />
           </div>
           <Row>
             <Col lg={12}>
@@ -63,6 +75,21 @@ const UnitList = () => {
                   <Card.Title>Product Unit</Card.Title>
                 </Card.Header>
                 <Card.Body>
+                  <div className="search_bar dropdown show mb-3">
+                    <div className="dropdown-menushow">
+                      <form onSubmit={(e) => e.preventDefault()}>
+                        <input
+                          className="form-control"
+                          type="search"
+                          placeholder="Search Unit"
+                          aria-label="Search"
+                          onChange={(event) =>
+                            setSearchTerm(event.target.value)
+                          }
+                        />
+                      </form>
+                    </div>
+                  </div>
                   <Table responsive>
                     <thead>
                       <tr>
@@ -84,48 +111,56 @@ const UnitList = () => {
                       </tr>
                     </thead>
                     <tbody>
-                      {Object.keys(unitObjects).map((id) => {
-                        return (
-                          <tr key={id}>
-                            <td>
-                              <div className="d-flex">
+                      {Object.keys(filteredUnit(unitList, searchTerm)).map(
+                        (id) => {
+                          return (
+                            <tr key={id}>
+                              <td>
+                                <div className="d-flex">
+                                  <Button
+                                    onClick={() => {
+                                      setCurrentId(id);
+                                      window.scrollTo({
+                                        top: 0,
+                                        behavior: "smooth",
+                                      });
+                                    }}
+                                    className="btn btn-primary btn-xs  mr-1"
+                                  >
+                                    Edit
+                                  </Button>
+                                </div>
+                              </td>
+                              <td>{unitList[id].unitName}</td>
+                              <td>{unitList[id].abbreviation}</td>
+                              <td>{unitList[id].dateAdded}</td>
+                              <td>
+                                {!unitList[id].isActive ? (
+                                  <Badge variant="danger light">
+                                    {" "}
+                                    INACTIVE{" "}
+                                  </Badge>
+                                ) : (
+                                  <Badge variant="primary light">
+                                    {" "}
+                                    ACTIVE{" "}
+                                  </Badge>
+                                )}
+                              </td>
+                              <td>
                                 <Button
                                   onClick={() => {
-                                    setCurrentId(id);
-                                    window.scrollTo({
-                                      top: 0,
-                                      behavior: "smooth",
-                                    });
+                                    onDelete(id);
                                   }}
-                                  className="btn btn-primary btn-xs  mr-1"
+                                  className="btn btn-danger btn-xs  mr-1"
                                 >
-                                  Edit
+                                  Delete
                                 </Button>
-                              </div>
-                            </td>
-                            <td>{unitObjects[id].unitName}</td>
-                            <td>{unitObjects[id].abbreviation}</td>
-                            <td>{unitObjects[id].dateAdded}</td>
-                            <td>
-                              {!unitObjects[id].isActive ? (
-                                <Badge variant="danger light"> INACTIVE </Badge>
-                              ) : (
-                                <Badge variant="primary light"> ACTIVE </Badge>
-                              )}
-                            </td>
-                            <td>
-                              <Button
-                                onClick={() => {
-                                  onDelete(id);
-                                }}
-                                className="btn btn-danger btn-xs  mr-1"
-                              >
-                                Delete
-                              </Button>
-                            </td>
-                          </tr>
-                        );
-                      })}
+                              </td>
+                            </tr>
+                          );
+                        }
+                      )}
                     </tbody>
                   </Table>
                 </Card.Body>
